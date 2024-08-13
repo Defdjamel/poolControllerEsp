00a79c1b0385a023e3cb598b126949e2ce1d2c08
james <guellati@gmail.com>
2024-08-13 20:30:15 +0200

    setting btn

--- src/views/SettingsView.h
+++ src/views/SettingsView.h
@@ -2,6 +2,11 @@
 static lv_obj_t * list1;
 void createSettingView(lv_obj_t *parent);
 
+
+ static char   Click_ph_calibration[] = "Click_ph_calibration";
+ static char  Click_orp_calibration[] = "Click_orp_calibration";
+ static char  Click_wifi_setting[] = "Click_wifi_setting";
+
 static void event_handler(lv_event_t * e)
 {
     lv_event_code_t code = lv_event_get_code(e);
@@ -9,7 +14,14 @@ static void event_handler(lv_event_t * e)
     if(code == LV_EVENT_CLICKED) {
         LV_UNUSED(obj);
         LV_LOG_USER("Clicked: %s", lv_list_get_button_text(list1, obj));
-        
+         LV_LOG_USER( "userdata : %s",(char*) obj->user_data);
+         //Serial.println((int*) obj->user_data);
+         if((char*) obj->user_data == Click_ph_calibration)
+          LV_LOG_USER("goto ph calibration");
+           else if((char*) obj->user_data == Click_orp_calibration)
+          LV_LOG_USER("goto Click_orp_calibration:");
+           else if((char*) obj->user_data == Click_wifi_setting)
+          LV_LOG_USER("goto Click_wifi_setting:");
     }
 }
 
@@ -32,16 +44,23 @@ void createSettingView(lv_obj_t *parent)
     
 
     /*Add buttons to the list*/
+    uint32_t type = 1;
     lv_obj_t * btn;
     lv_list_add_text(list1, "Sondes");
     btn = lv_list_add_button(list1, LV_SYMBOL_SETTINGS, "Calibration PH");
-    lv_obj_add_event_cb(btn, event_handler, LV_EVENT_CLICKED, NULL);
-     btn = lv_list_add_button(list1, LV_SYMBOL_SETTINGS, "Calibration Chlore");
-    lv_obj_add_event_cb(btn, event_handler, LV_EVENT_CLICKED, NULL);
+    lv_obj_set_user_data(btn, &Click_ph_calibration);
+    lv_obj_add_event_cb(btn, event_handler, LV_EVENT_CLICKED,NULL);
+   
+    
+    btn = lv_list_add_button(list1, LV_SYMBOL_SETTINGS, "Calibration Chlore");
+    lv_obj_set_user_data(btn, &Click_orp_calibration);
+    lv_obj_add_event_cb(btn, event_handler, LV_EVENT_CLICKED,  NULL);
+     
    
 
     lv_list_add_text(list1, "Connection");
     btn = lv_list_add_button(list1, LV_SYMBOL_WIFI, "Wifi");
+    lv_obj_set_user_data(btn, &Click_wifi_setting);
     lv_obj_add_event_cb(btn, event_handler, LV_EVENT_CLICKED, NULL);
     
 
